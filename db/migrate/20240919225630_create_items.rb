40d521f12070d75a6e1d503a2e052f185e30efb4
hiroshi-43 <piropirorock@gmail.com>
2024-09-28 18:33:10 +0900

    マイグレーション調整中

--- db/migrate/20240919225630_create_items.rb
+++ db/migrate/20240919225630_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-
       t.string  :item_name, null: false
       t.text  :item_text, null: false
       t.integer  :price, null: false
@@ -11,7 +10,6 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer  :ship_cost_id, null: false
       t.integer  :deli_time_id , null: false
       t.references :user, null: false, foreign_key: true
-      
       t.timestamps
     end
   end
