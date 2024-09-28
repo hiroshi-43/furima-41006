4f82c54f4d00f93851efcf128ca44115a895551d
hiroshi-43 <piropirorock@gmail.com>
2024-09-28 23:35:40 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :uesr
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :deli_time
+  belongs_to :ship_cost
+
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :price, presence: true
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :deli_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_cost_id, numericality: { other_than: 1, message: "can't be blank" }
 end
