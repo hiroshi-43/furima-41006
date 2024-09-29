03750c508c32e7f4ec426579a9ad00d443a3215b
hiroshi-43 <piropirorock@gmail.com>
2024-09-30 07:20:40 +0900

    販売価格のJavascript実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,28 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.new(item_params)
+    @item.user = current_user # 作成するアイテムにユーザーを追加
+    if @item.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :item_name,
+      :item_text,
+      :price,
+      :prefecture_id,
+      :category_id,
+      :condition_id,
+      :deli_time_id,
+      :ship_cost_id,
+      :image
+    )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
-  belongs_to :uesr
-  belongs_to :prefecture
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :deli_time
-  belongs_to :ship_cost
+  belongs_to :user
 
-  validates :item_name, presence: true
-  validates :item_text, presence: true
-  validates :price, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :deli_time
+  belongs_to_active_hash :ship_cost
+
+  has_one_attached :image
+
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :item_text, presence: true, length: { maximum: 1000 }
+  validates :image, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
