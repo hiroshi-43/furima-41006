4a816b8bddf3a9a1f3ed806be0d7baeeae03a87d
hiroshi-43 <piropirorock@gmail.com>
2024-10-04 14:47:09 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_item, only: [:edit, :update]
-  before_action :authorize_user, only: [:edit, :update]
+  before_action :authorize_user, only: [:edit, :update, :show]
 
   def index
     @items = Item.order(created_at: :desc)
