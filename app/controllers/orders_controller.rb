5d8a9b8566d5b44877ec6cfa57752a4b2d53f9c3
hiroshi-43 <piropirorock@gmail.com>
2024-10-09 11:58:47 +0900

    クレジット以外の購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :redirect_if_item_sold_or_owned, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
@@ -8,13 +9,11 @@ class OrdersController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-    Rails.logger.info "Parameters: #{params.inspect}" # デバッグ用ログ出力
     @order = ItemAddress.new(order_params.merge(user_id: current_user.id, item_id: @item.id))
 
     if @order.save
-      redirect_to item_path(@item), notice: 'Order was successfully created.'
+      redirect_to root_path, notice: 'Order was successfully created.'
     else
-      Rails.logger.info "Order errors: #{@order.errors.full_messages}" # エラーメッセージのログ出力
       render :index, status: :unprocessable_entity
     end
   end
@@ -22,7 +21,22 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:item_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_num, :user_id,
-                                         :item_id)
+    params.require(:item_address).permit(
+      :postal_code,
+      :prefecture_id,
+      :city, :address,
+      :building_name,
+      :phone_num,
+      :user_id,
+      :item_id
+    )
+  end
+
+  def redirect_if_item_sold_or_owned
+    @item = Item.find(params[:item_id])
+    # 出品者である場合、または売却済みの場合にトップページにリダイレクト
+    return unless @item.user_id == current_user.id || @item.sold_out?
+
+    redirect_to root_path, notice: 'この商品にはアクセスできません'
   end
 end
